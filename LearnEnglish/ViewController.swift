9188113ea648f395600d0e7d572a46bcebea45fa
Tomasz Twardowski <tomasztwardowski@Tomaszs-MacBook-Pro.local>
2016-10-27 18:43:24 +0200

    Initial Commit

---
+++ LearnEnglish/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnEnglish
+//
+//  Created by Tomasz Twardowski on 27/10/2016.
+//  Copyright © 2016 Tomasz Twardowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
